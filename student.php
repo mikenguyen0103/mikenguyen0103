81657d256cd3dcf939c7aee8947156d0c4778e8f
mikenguyen0103 <148010311+mikenguyen0103@users.noreply.github.com>
2024-01-14 20:46:36 -0800

    Create student.php

---
+++ student.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Student Page</title>
+</head>
+<body>
+    <?php
+    $studentName = "Khang Nguyen";
+    echo ("Welcome, $studentName");
+    ?>
+</body>
+</html>
